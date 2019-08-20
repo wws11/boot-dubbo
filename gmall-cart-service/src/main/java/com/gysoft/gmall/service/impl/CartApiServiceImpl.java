5618ffd66801fa7864bc035063cb9cedf740d862
wws11 <17637900215@163.com>
2019-08-20 19:28:36 +0800

    整合了Redistemplate

--- gmall-cart-service/src/main/java/com/gysoft/gmall/service/impl/CartApiServiceImpl.java
+++ gmall-cart-service/src/main/java/com/gysoft/gmall/service/impl/CartApiServiceImpl.java
@@ -2,6 +2,9 @@ package com.gysoft.gmall.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.gysoft.gmall.cart.api.CartApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
 
 /**
  * @author 魏文思
@@ -9,8 +12,16 @@ import com.gysoft.gmall.cart.api.CartApiService;
  */
 @Service
 public class CartApiServiceImpl  implements CartApiService {
+    @Autowired
+    RedisTemplate redisTemplate;
+
     @Override
     public void test() {
         System.out.println("cart  success ！！！");
+        //这里相当于redis对String类型的set操作
+        redisTemplate.opsForValue().set("test","helloworld");
+        //这里相当于redis对String类型的get操作
+        String test = (String)redisTemplate.opsForValue().get("test");
+        System.out.println(test);
     }
 }
