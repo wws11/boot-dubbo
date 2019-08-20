b2bca99e5e495f73ccaac807dae5b309240d1536
wws11 <17637900215@163.com>
2019-08-20 15:54:58 +0800

    微调

--- gmall-cart-service/src/main/java/com/gysoft/gmall/DubboCartServiceApplication.java
+++ gmall-cart-service/src/main/java/com/gysoft/gmall/DubboCartServiceApplication.java
@@ -1,6 +1,5 @@
 package com.gysoft.gmall;
 
-import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- gmall-provider/src/main/java/com/gysoft/gmall/DubboProducerApplication.java
+++ gmall-provider/src/main/java/com/gysoft/gmall/DubboProducerApplication.java
@@ -1,6 +1,5 @@
 package com.gysoft.gmall;
 
-import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author 魏文思
  * @date 2019/8/20$ 9:14$
  */
-@DubboComponentScan(basePackages = "com.gysoft.gmall.service.impl")
 @SpringBootApplication()
 @EnableDubbo
 public class DubboProducerApplication {
